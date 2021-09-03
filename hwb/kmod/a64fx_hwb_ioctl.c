a4ff3dda91e6def4868350edae402a357cc826e1
Thomas Roehl <Thomas.Roehl@googlemail.com>
2021-09-03 08:36:27 -0400

    Fix for get_peinfo on A64FX

--- hwb/kmod/a64fx_hwb_ioctl.c
+++ hwb/kmod/a64fx_hwb_ioctl.c
@@ -215,7 +215,7 @@ static int _oss_a64fx_hwb_get_peinfo(u8* cmg, u8 * ppe)
 static int _oss_a64fx_hwb_get_peinfo(u8* cmg, u8 * ppe)
 {
     u64 val = 0;
-    asm ("MRS %0, S3_0_C11_C12_4" :: "r"(val));
+    asm volatile ("MRS %0, S3_0_C11_C12_4" : "=r"(val) );
     *ppe = (u8)(val & 0xF);
     *cmg = (u8)((val >> 4) & 0x3);
     pr_info("get_peinfo for CPU %d CMG%u PPE %u Raw 0x%llX\n", smp_processor_id(), *cmg, *ppe, val);
