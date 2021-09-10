a9c538c3d0b0db344ed672135a57d151c0c4d3ee
Thomas Roehl <Thomas.Roehl@googlemail.com>
2021-09-10 08:09:54 -0400

    Some work on the IOCTL calls. allocate/free work, assign has problems. Add reset IOCTL

--- hwb/kmod/a64fx_hwb_ioctl.h
+++ hwb/kmod/a64fx_hwb_ioctl.h
@@ -3,15 +3,20 @@
 
 #include "a64fx_hwb.h"
 
+#define FUJITSU_HWB_IOC_RESET _IOWR(__FUJITSU_IOCTL_MAGIC, 0x05, int)
+
 int oss_a64fx_hwb_get_peinfo(int *cmg, int *ppe);
 int oss_a64fx_hwb_get_peinfo_ioctl(unsigned long arg);
-int oss_a64fx_hwb_allocate(struct a64fx_hwb_device *dev, unsigned long arg);
-int oss_a64fx_hwb_free(struct a64fx_hwb_device *dev, int cmg_id, int bb_id);
+/*int oss_a64fx_hwb_allocate(struct a64fx_hwb_device *dev, unsigned long arg);*/
+int oss_a64fx_hwb_allocate_ioctl(struct a64fx_hwb_device *dev, unsigned long arg);
+/*int oss_a64fx_hwb_free(struct a64fx_hwb_device *dev, int cmg_id, int bb_id);*/
 int oss_a64fx_hwb_free_ioctl(struct a64fx_hwb_device *dev, unsigned long arg);
-int oss_a64fx_hwb_assign_blade(struct a64fx_hwb_device *dev, int blade, int window);
+/*int oss_a64fx_hwb_assign_blade(struct a64fx_hwb_device *dev, int blade, int window);*/
 int oss_a64fx_hwb_assign_blade_ioctl(struct a64fx_hwb_device *dev, unsigned long arg);
-int oss_a64fx_hwb_unassign_blade(struct a64fx_hwb_device *dev, int bb, int window);
+/*int oss_a64fx_hwb_unassign_blade(struct a64fx_hwb_device *dev, int bb, int window);*/
 int oss_a64fx_hwb_unassign_blade_ioctl(struct a64fx_hwb_device *dev, unsigned long arg);
+
+int oss_a64fx_hwb_reset_ioctl(struct a64fx_hwb_device *dev, unsigned long arg);
 
 struct a64fx_task_mapping * get_taskmap(struct a64fx_hwb_device *dev, struct task_struct* task);
 int unregister_task(struct a64fx_hwb_device *dev, struct a64fx_task_mapping *taskmap);
